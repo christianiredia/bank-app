323bad04d8a37b8e9dc0e39b3c476658dca0d46b
Christian Iredia <101851819+christianiredia@users.noreply.github.com>
2023-02-21 00:01:38 -0600

    for the close account function, if the account username and pin are equal then the account will be closed

--- index.js
+++ index.js
@@ -172,3 +172,17 @@ btnTransfer.addEventListener("click", function (e) {
     updateUi(currentAccount);
   }
 });
+
+btnClose.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
+    accounts.splice(index, 1);
+  }
+});
